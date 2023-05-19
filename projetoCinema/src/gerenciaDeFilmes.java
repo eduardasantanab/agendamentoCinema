c7b91480acf6127b704825fc733d6e7612988a75
Eduarda Santana <88462631+eduardasantanab@users.noreply.github.com>
2023-05-19 19:47:47 -0300

    Delete projetoCinema/src directory

--- projetoCinema/src/gerenciaDeFilmes.java
+++
@@ -1,7 +0,0 @@
-public interface gerenciaDeFilmes {
-
-    void incluirFilme(String nomeDoFilme, String duracaoDoFilme, String sinopse, double valor);
-    void excluirFilme(String target);
-    void alterarFilme(String nomeFilme);
-
-}
